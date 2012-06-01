4c3e72bb26647fd913ada21388dbd3c6d984efa0
Tyson DeNio <d3ltak@gmail.com>
2012-06-01 15:59:19 -0400

    Added basic block events

--- src/main/java/com/d3lta/paladin/listener/PaladinBlockListener.java
+++ src/main/java/com/d3lta/paladin/listener/PaladinBlockListener.java
@@ -1,13 +1,43 @@
 package com.d3lta.paladin.listener;
 
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 import com.d3lta.paladin.Paladin;
 
 public class PaladinBlockListener implements Listener {
 
+	Paladin paladin;
 	
 	public PaladinBlockListener(Paladin plugin) {
-		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		
+		this.paladin = plugin;
+		
+		paladin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
+	
+	public void onBlockDamage(BlockDamageEvent event) {
+		
+	}
+	
+	public void onBlockBreak(BlockBreakEvent event) {
+		
+	}
+	
+	public void onBlockPlace(BlockPlaceEvent event) {
+		
+	}
+	
+	public void onBlockPistonExtend(BlockPistonExtendEvent event) {
+		
+	}
+	
+	public void onBlockPistonRetract(BlockPistonRetractEvent event) {
+		
+	}
+	
 }
